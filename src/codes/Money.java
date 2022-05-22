72b26155937438edd51db9d66bac7a6bcae6aa0a
danieldougr <danieldougr@gmail.com>
2022-05-22 16:03:11 -0300

    Quinto commit

--- src/codes/Money.java
+++ src/codes/Money.java
@@ -12,30 +12,26 @@ abstract class Money {
     return currency;
     }
 
-    Money times(int multiplier)  {
-        return Money.franc(amount * multiplier);
-     }
-
-     static Money dollar(int amount)  {
-        return new Dollar(amount, "USD");
+    static Money dollar(int amount)  {
+        return new Money(amount, "USD");
      }
   
      static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
      }
-  
+      
      Money(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
+     }
+      
+     public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && currency().equals(money.currency());
+     }
+      
+     Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
      }    
-
-    abstract Money times(int multiplier);   
-   
-   public boolean equals(Object object) {
-    Money money = (Money) object;
-    return amount == money.amount && getClass().equals(money.getClass());
-    }
-
-    
 
 }
