c62a950675df140b80b2e4cb72d3dfefecafaceb
danieldougr <danieldougr@gmail.com>
2022-05-22 15:40:00 -0300

    Segundo commit

---
+++ src/codes/Dollar.java
@@ -0,0 +1,31 @@
+package codes;
+
+public class Dollar {
+
+   private int amount;
+
+   Dollar(int amount) {
+      this.amount= amount;
+   }
+   void times(int multiplier) {
+      amount= amount * multiplier;
+   }
+
+   Dollar times(int multiplier) {
+      return new Dollar(amount * multiplier);
+   }
+  
+  public void testMultiplication() {
+     Dollar five = new Dollar(5);
+     Dollar product = five.times(2);
+     assertEquals(10, product.amount);
+     product = five.times(3);
+     assertEquals(new Dollar(15), product);
+   }
+   
+   public boolean equals(Object object)  {
+      Dollar dollar = (Dollar) object;
+      return amount == dollar.amount;
+   }
+}
+
